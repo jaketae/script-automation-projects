675d7b1f5c1660a1ccf2124016c4731fda361ceb
“jaketae” <jaesungtae@gmail.com>
2020-03-18 08:05:46 +0900

    Update code with GitHub access token

--- repo-initializer/create.py
+++ repo-initializer/create.py
@@ -2,15 +2,14 @@ import sys
 import os
 from github import Github
 
-username = "jaketae"
-password = "" 
+access_token = "your_token"
 path = "/Users/jaketae/documents/github"
 
 
 def create():
 	project_name = str(sys.argv[1])
 	os.makedirs(os.path.join(path, project_name))
-	user = Github(username, password).get_user()
+	user = Github(access_token).get_user()
 	user.create_repo(project_name)
 
 if __name__ == '__main__':
