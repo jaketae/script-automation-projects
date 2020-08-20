3c1aba9ff38ad17dd399d12c79f955837c1d8605
Jake Tae <jaesungtae@gmail.com>
2020-08-20 22:06:37 +0900

    feature: update .gitignore

--- repo-initializer/create.py
+++ repo-initializer/create.py
@@ -1,16 +1,16 @@
-import sys
 import os
+import sys
+
 from github import Github
 
-access_token = "your_access_token"
-path = "/Users/jaketae/documents/github"
+
+def create(path):
+    project_name = str(sys.argv[1])
+    os.makedirs(os.path.join(path, project_name))
+    user = Github(os.environ["GITHUB_TOKEN"]).get_user()
+    user.create_repo(project_name)
 
 
-def create():
-	project_name = str(sys.argv[1])
-	os.makedirs(os.path.join(path, project_name))
-	user = Github(access_token).get_user()
-	user.create_repo(project_name)
-
-if __name__ == '__main__':
-	create()
+if __name__ == "__main__":
+    path = "/Users/jaketae/documents/dev/github"
+    create(path)
